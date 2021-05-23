7804d28788298ae051979188495a50f0bd52a6cd
Juti Buican <jbuican19@gmail.com>
2021-05-23 21:19:28 +0100

    Add style module to eventList and eventItem

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,10 +1,11 @@
 import EventItem from './event-item';
+import classes from './event-list.module.css';
 
 function EventList(props) {
   const { items } = props;
-  console.log(items);
+
   return (
-    <ul>
+    <ul className={classes.list}>
       {items.map((event) => (
         <EventItem
           key={event.id}
