c7d2d8756b8e8066daa2680ac3a00da9f108f96f
Juti Buican <jbuican19@gmail.com>
2021-05-27 22:20:48 +0100

    Edit* event content, logistics and summary

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,14 @@
+import { useRouter } from 'next/router';
+import { getEventById } from '../../dummy-data';
 function EventDetailPage() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(getEventById);
+  if (!event) {
+    return <p>No Event Found</p>;
+  }
+
   return (
     <div>
       <h1>Event Detail Page</h1>
