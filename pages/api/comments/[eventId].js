deb17e7db22902fb82b60ba38d50601a24b72051
Juti Buican <jbuican19@gmail.com>
2021-09-12 22:57:49 +0100

    updated code to use util db, db is not commited to git

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,5 +1,20 @@
-function handler(req, res) {
+import {
+  connectDatabase,
+  insertDocument,
+  getAllDocuments,
+} from "../../../helpers/db-util";
+
+async function handler(req, res) {
   const eventId = req.query.eventId;
+
+  let client;
+
+  try {
+    client = await connectDatabase();
+  } catch (error) {
+    res.status(500).json({message: "Connecting to the database failed!"});
+    return;
+  }
 
   if (req.method === "POST") {
     // add server side validation
@@ -13,26 +28,38 @@ function handler(req, res) {
       text.trim() === ""
     ) {
       res.status(422).json({message: "Invalid input."});
+      client.close();
       return;
     }
 
     const newComment = {
-      id: new Date().toISOString(),
       email,
       name,
       text,
+      eventId,
     };
-    console.log(newComment);
-    res.status(201).json({message: "Added comment.", comment: newComment});
+    // console.log(newComment);
+    let result;
+
+    try {
+      result = await insertDocument(client, "comments", newComment);
+      newComment._id = result.insertedId;
+      res.status(201).json({message: "Added comment.", comment: newComment});
+    } catch (error) {
+      res.status(500).json({message: "Inserting comment failed!"});
+    }
   }
 
   if (req.method === "GET") {
-    const dummyList = [
-      {id: "c1", name: "Juti", text: "My first comment"},
-      {id: "e2", name: "Sandy", text: "My second comment"},
-    ];
-    res.status(200).json({comments: dummyList});
+    try {
+      const documents = await getAllDocuments(client, "comments", {_id: -1});
+      res.status(200).json({comments: documents});
+    } catch (error) {
+      res.status(500).json({message: "Getting Comments failed."});
+    }
   }
+
+  client.close();
 }
 
 export default handler;
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,7 @@
-function handler(req, res) {
+import {connectDatabase, insertDocument} from "../../helpers/db-util";
+
+async function handler(req, res) {
+  console.log(" newsletter");
   if (req.method === "POST") {
     const userEmail = req.body.email;
 
@@ -6,8 +9,23 @@ function handler(req, res) {
       res.status(422).json({message: "Invalid email address."});
       return;
     }
+    let client;
 
-    console.log(userEmail);
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({message: "Connecting to the database failed!"});
+      return;
+    }
+
+    try {
+      await insertDocument(client, "newsletter", {email: userEmail});
+      client.close();
+    } catch (error) {
+      res.status(500).json({message: "Inserting data failed!"});
+      return;
+    }
+
     res.status(201).json({message: "Signed up!"});
   }
 }
