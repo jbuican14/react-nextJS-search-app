ee4c2e7b8d825a8c3ebebe90af60bd6f3b158376
Juti Buican <jbuican19@gmail.com>
2021-09-12 18:10:05 +0100

    Comment, MongoDB Atlas

---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,38 @@
+function handler(req, res) {
+  const eventId = req.query.eventId;
+
+  if (req.method === "POST") {
+    // add server side validation
+    const {email, name, text} = req.body;
+
+    if (
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !text ||
+      text.trim() === ""
+    ) {
+      res.status(422).json({message: "Invalid input."});
+      return;
+    }
+
+    const newComment = {
+      id: new Date().toISOString(),
+      email,
+      name,
+      text,
+    };
+    console.log(newComment);
+    res.status(201).json({message: "Added comment.", comment: newComment});
+  }
+
+  if (req.method === "GET") {
+    const dummyList = [
+      {id: "c1", name: "Juti", text: "My first comment"},
+      {id: "e2", name: "Sandy", text: "My second comment"},
+    ];
+    res.status(200).json({comments: dummyList});
+  }
+}
+
+export default handler;
