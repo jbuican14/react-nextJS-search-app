9de33a20cdc6064abfc538c661aee3ed86bf4802
Juti Buican <jbuican19@gmail.com>
2021-09-17 23:53:25 +0100

    removed commented code

--- components/input/comments.js
+++ components/input/comments.js
@@ -16,46 +16,19 @@ function Comments(props) {
   useEffect(() => {
     if (showComments) {
       setIsFetchingComments(true);
-      // notificationCtx.showNotification({
-      //   title: "Loading comments...",
-      //   message: "Loading comments.",
-      //   status: "pending",
-      // });
       fetch("/api/comments/" + eventId)
         .then((response) => {
           return response.json();
-          // if (response.ok) {
-          //   return response.json();
-          // }
-          // return response.json().then((data) => {
-          //   throw new Error(data.message || "Something went wrong");
-          // });
         })
         .then((data) => {
-          console.log("data", data);
           setIsFetchingComments(false);
-          // notificationCtx.showNotification({
-          //   title: "Success!!",
-          //   message: "Successfully loaded  comments.",
-          //   status: "success",
-          // });
           setComments(data.comments);
         });
-      // .catch((error) => {
-      //   notificationCtx.showNotification({
-      //     title: "Error",
-      //     message: error.message || "Opps something went wrong.",
-      //     status: "error",
-      //   });
-      // });
     }
   }, [showComments]);
 
   function toggleCommentsHandler() {
     setShowComments((prevStatus) => !prevStatus);
-    // if(!showComments) {
-
-    // }
   }
 
   function addCommentHandler(commentData) {
